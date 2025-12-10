36b3d43519434794345e0a614dab592e4d8a4598
Eiad Soufan <eiad.soufan.2@gmail.com>
2025-12-10 18:21:07 +0800

    Update wsgi.py

--- model_system/wsgi.py
+++ model_system/wsgi.py
@@ -5,10 +5,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'model_system.settings')
 
 application = get_wsgi_application()
-
-# المسار الصحيح الذي يستخدمه Render بعد collectstatic
-STATIC_ROOT = '/opt/render/project/src/staticfiles'
-
-# إضافة ملفات static
 application = WhiteNoise(application)
-application.add_files(STATIC_ROOT, prefix='static/')
