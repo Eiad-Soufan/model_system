d19af790fd481b8341821e00e164151d596a0efb
Eiad Soufan <eiad.soufan.2@gmail.com>
2025-12-11 02:17:44 +0800

    Update urls.py

--- model_system/urls.py
+++ model_system/urls.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
 from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),  # هذا يحتوي على MyTokenObtainPairView
-] 
+    path('api/', include('core.urls')),  # يحتوي كل الواجهات
+]
 
+# 👇 أثناء التطوير (DEBUG=True) استعمل helper العادي
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
+    # 👇 في الإنتاج (DEBUG=False) نخدم /media/ يدويًا
     urlpatterns += [
-        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     ]
