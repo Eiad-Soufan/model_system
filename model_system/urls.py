7a18f462d20180b9a858545bdaa88be315681510
Eiad Soufan <eiad.soufan.2@gmail.com>
2025-12-11 02:14:57 +0800

    Update urls.py

--- model_system/urls.py
+++ model_system/urls.py
@@ -6,4 +6,11 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),  # هذا يحتوي على MyTokenObtainPairView
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
